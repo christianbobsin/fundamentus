53c94aeb87bf037aa16af1ceca8f588523b58fd1
christianbobsin <christianbobsin@gmail.com>
2023-09-14 00:50:35 -0300

    ajustando cnexoes simultaneas

--- cotations/cotations/spiders/fundamentus.py
+++ cotations/cotations/spiders/fundamentus.py
@@ -1,4 +1,5 @@
 import scrapy
+import time
 
 
 
@@ -16,13 +17,18 @@ class FundamentusSpider(scrapy.Spider):
         table = response.xpath("/html/body/div[1]/div[2]/div/div/table/tbody")
         rows = table.xpath(".//tr")
 
+        count = 0
         for row in rows:
             value = row.xpath(".//td[1]/a/text()").extract_first()
      
-            #print(value)
+            count += 1
+
+            if  ((count % 5 ) == 0):
+                time.sleep(1.4)
 
             yield scrapy.Request(
-                url='http://www.fundamentus.com.br/detalhes.php?papel=%s' % value.strip(),
+                #url='http://www.fundamentus.com.br/detalhes.php?papel=%s' % value.strip(),
+                url='https://statusinvest.com.br/acoes/%s' % value.strip(),
                 callback= self.parse_detail
             )            
 
@@ -32,10 +38,10 @@ class FundamentusSpider(scrapy.Spider):
 
     def parse_detail(self, response):
         
-        name = response.xpath('//td[2]/span/text()').extract_first()
-        cotation = response.xpath('//td[@class="data destaque w3"]/span/text()').extract_first()
-        p_vp = response.xpath('/html/body/div[1]/div[2]/table[3]/tbody/tr[2]/td[4]/span/text()').extract_first()
-        dividend_yeld = response.xpath('//tbody/tr[9]/td[4]/span/text()').extract_first()
+        name = response.xpath('/html/body/main/header/div[2]/div/div[1]/div/ol/li[3]/a/span/text()').extract_first()
+        cotation = response.xpath('/html/body/main/div[2]/div/div[1]/div/div[1]/div/div[1]/strong/text()').extract_first()
+        p_vp = response.xpath('/html/body/main/div[2]/div/div[8]/div[2]/div/div[1]/div/div[4]/div/div/strong/text()').extract_first()
+        dividend_yeld = response.xpath('/html/body/main/div[2]/div/div[8]/div[2]/div/div[1]/div/div[1]/div/div/strong/text()').extract_first()
         
         yield {
             "name": name,
@@ -47,4 +53,23 @@ class FundamentusSpider(scrapy.Spider):
    
         #print ( " cotation: {cotation} / p/vp: {p_vp}")
         
+
+
+
+
+#Status Invest
+
+#name
+#/html/body/main/header/div[2]/div/div[1]/div/ol/li[3]/a/span
+
+#cotation
+#/html/body/main/div[2]/div/div[1]/div/div[1]/div/div[1]/strong
+
+#p_vp
+#/html/body/main/div[2]/div/div[8]/div[2]/div/div[1]/div/div[4]/div/div/strong
+
+#d_y
+#/html/body/main/div[2]/div/div[8]/div[2]/div/div[1]/div/div[1]/div/div/strong
+
+
 
